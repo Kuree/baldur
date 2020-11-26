f3301db9ccfbdffe9bf14382ed09385d22712a8e
Keyi Zhang <keyi@stanford.edu>
2020-11-25 16:28:02 -0800

    need to figure out the best way to implement ast visitor

--- src/sv.cc
+++ src/sv.cc
@@ -1,28 +1,55 @@
 #include "sv.hh"
 
 #include <filesystem>
+#include <type_traits>
 
 #include "fmt/format.h"
 #include "slang/compilation/Compilation.h"
 #include "slang/parsing/Preprocessor.h"
+#include "slang/symbols/CompilationUnitSymbols.h"
+#include "slang/symbols/Symbol.h"
 #include "slang/syntax/SyntaxTree.h"
 #include "slang/text/SourceManager.h"
+#include "slang/symbols/ASTVisitor.h"
 
 namespace fs = std::filesystem;
 
+// visitor to visit all the symbols if possible
+class SymbolVisitor {
+public:
+    SymbolVisitor(slang::Compilation &compilation, baldur::Symbol &root)
+        : compilation_(compilation), root_(root) {}
+
+    template <typename T>
+    void visit(const T &elem) {
+        // depends on the type
+        if constexpr (std::is_base_of_v<slang::Statement, T>) {
+        } else if constexpr (std::is_base_of_v<slang::Expression, T>) {
+        } else if constexpr (std::is_base_of_v<slang::ValueSymbol, T>) {
+        }
+        else if  constexpr (std::is_base_of_v<slang::Scope, T>) {
+            if (!elem.empty()) {
+                for (auto const &mem: elem.members())
+                    visit(mem);
+            }
+        }
+    }
+
+private:
+    slang::Compilation &compilation_;
+    baldur::Symbol &root_;
+};
+
+
 namespace baldur {
-std::unique_ptr<Symbol> parse_sv(const std::vector<std::string>& files,
-                                 const std::vector<std::string>& defines,
-                                 const std::vector<std::string>& includes) {
-    // stack-allocated source manager seems to get segfault sometimes
-    // with gcc-9+ when destructor is called
+std::unique_ptr<Symbol> parse_sv(const std::vector<std::string> &files,
+                                 const std::vector<std::string> &defines,
+                                 const std::vector<std::string> &includes) {
     slang::SourceManager source_manager;
-    // macro
     slang::PreprocessorOptions ppoptions;
     ppoptions.predefines = defines;
-    ppoptions.predefineSource = "<command-line>";
 
-    for (auto const& dirname : includes) {
+    for (auto const &dirname : includes) {
         if (!fs::exists(dirname)) {
             throw std::runtime_error(fmt::format("{0} does not exists", dirname));
         }
@@ -31,7 +58,7 @@ std::unique_ptr<Symbol> parse_sv(const std::vector<std::string>& files,
 
     std::vector<slang::SourceBuffer> buffers;
     buffers.reserve(files.size());
-    for (auto const& filename : files) {
+    for (auto const &filename : files) {
         if (!fs::exists(filename)) {
             throw std::runtime_error(fmt::format("{0} does not exists", filename));
         }
@@ -45,11 +72,15 @@ std::unique_ptr<Symbol> parse_sv(const std::vector<std::string>& files,
     slang::Bag options;
     options.set(ppoptions);
     slang::Compilation compilation(options);
-    for (auto const &buffer: buffers) {
+    for (auto const &buffer : buffers) {
         compilation.addSyntaxTree(slang::SyntaxTree::fromBuffer(buffer, source_manager, options));
     }
 
     // now construct the symbol tree
-    return nullptr;
+    auto const &root = compilation.getRoot();
+    auto result = std::make_unique<Symbol>();
+    SymbolVisitor visitor(compilation, *result);
+    visitor.visit(root);
+    return result;
 }
 }  // namespace baldur
